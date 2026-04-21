52d7f4be290a0dc669cc941afb478055a6ba1f8f
venkatanuvishwa-sys <venkatanuvishwa@gmail.com>
2026-04-21 23:07:23 +0530

    Create ProjectPage.ts file with ProjectPage class methods

---
+++ src/pages/ProjectPage.ts
@@ -0,0 +1,27 @@
+class ProjectPage {
+  createProject(projectDetails) {
+    // Implementation for creating a project
+  }
+
+  deleteProject(projectId) {
+    // Implementation for deleting a project
+  }
+
+  updateProject(projectId, projectDetails) {
+    // Implementation for updating a project
+  }
+
+  getProject(projectId) {
+    // Implementation for retrieving a single project
+  }
+
+  getAllProjects() {
+    // Implementation for retrieving all projects
+  }
+
+  addTaskToProject(projectId, taskDetails) {
+    // Implementation for adding a task to a project
+  }
+}
+
+export default ProjectPage;

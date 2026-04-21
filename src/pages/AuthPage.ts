050b11741b847db0a75a9b94c754c3b35cef1a3a
venkatanuvishwa-sys <venkatanuvishwa@gmail.com>
2026-04-21 23:07:37 +0530

    Create AuthPage.ts file with AuthPage class and methods

---
+++ src/pages/AuthPage.ts
@@ -0,0 +1,31 @@
+class AuthPage {
+    navigateToLoginPage() {
+        // Logic to navigate to the login page
+    }
+
+    navigateToRegisterPage() {
+        // Logic to navigate to the register page
+    }
+
+    login(username, password) {
+        // Logic to log in the user
+    }
+
+    register(username, password) {
+        // Logic to register a new user
+    }
+
+    logout() {
+        // Logic to log out the user
+    }
+
+    isLoggedIn() {
+        // Logic to check if the user is logged in
+    }
+
+    getErrorMessage() {
+        // Logic to retrieve error messages
+    }
+}
+
+export default AuthPage;
